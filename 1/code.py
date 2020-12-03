1428d46b2d3b515c910d0b1758ea5273cdf8c043
Sujatha Nagarajan <sujatha.wgp@gmail.com>
2020-12-02 18:15:24 -0600

    day 1
    day 1

---
+++ 1/code.py
@@ -0,0 +1,24 @@
+class BreakOut(Exception): pass
+
+with open("input.txt","r") as file:
+    entries = file.readlines()
+    entries_copy = entries
+    
+    try:
+        for i in range(0, len(entries)):
+            for j in range(i+1, len(entries)):
+                if int(entries[i]) + int(entries[j])== 2020:
+                    print(f'a) {entries[i]} {entries[j]} {int(entries[i])*int(entries[j]) }')
+                    raise BreakOut
+    except BreakOut:
+        pass
+
+    try:
+        for i in range(0, len(entries)):
+          for j in range(i+1, len(entries)):
+                for k in range(j+1, len(entries)):
+                    if int(entries[i]) + int(entries[j]) + int(entries[k]) == 2020:
+                        print(f'\nb) {entries[i]} {entries[j]}  {entries[k]}  {int(entries[i]) * int(entries[j]) * int(entries[k])}')
+                        raise BreakOut
+    except BreakOut:
+        pass
