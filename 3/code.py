c9f0bba0e8b014f7028b1e1d44d54b8ce6bf7325
Sujatha Nagarajan <sujatha.wgp@gmail.com>
2020-12-05 01:01:51 -0600

    day 5
    day 5

---
+++ 3/code.py
@@ -0,0 +1,40 @@
+from util import *
+
+trees_b = 0
+
+slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+
+def find_trees_count(slope):
+    trees = 0
+
+    # get sanitized file contents as a list
+    entries = get_sanitized_file_contents("input.txt")
+    
+    # set first row along the slope
+    row = slope[1] # y direction
+
+    # get width of the input line which would repeat itself according to the puzzle
+    width = len(entries[0])
+
+    # set the column value
+    col = slope[0] % width # x direction
+
+    while row < len(entries):
+        
+        # if '#' is present then it is a tree
+        if entries[row][col] == '#':
+            trees += 1
+        
+        #update row and col
+        col = (col + slope[0]) % width
+        row += slope[1]
+    return trees
+
+
+final_value = 1
+for slope in slopes:
+    value = find_trees_count(slope)
+    print("for slope: " + str(slope) + " = " + str(value))
+    final_value *= value
+
+print("b) " + str(final_value))
---
+++ 5/code.py
@@ -0,0 +1,40 @@
+# advent of code
+# day 5
+
+import re
+
+highest_seat_id_a = 0
+
+
+def find_index(text, max_value, upper_half_string, lower_half_string):
+    start_index = 0 
+    end_index = max_value - 1
+
+    # find the single entry index based on string with a pattern
+    for r in text:
+        max_value = max_value/2
+        if r == upper_half_string:
+            end_index = start_index + max_value - 1
+        elif r == lower_half_string:
+            start_index = end_index - max_value + 1
+    return end_index
+
+list_of_seat_ids = []
+with open("input.txt","r") as file:
+    entries = file.readlines()
+
+    # first puzzle
+    for i in range(0, len(entries)):
+        e = entries[i]
+        row_string = e[:7]
+        seat_id = find_index(e[:7], 128, 'F', 'B') * 8 + find_index(e[7:], 8, 'L', 'R')
+        list_of_seat_ids.append(seat_id)        
+        highest_seat_id_a = int(seat_id) if seat_id > highest_seat_id_a else highest_seat_id_a
+    
+print("a) seat id " + str(int(highest_seat_id_a)))
+# find your seat id which is missing from the input file
+print("b) " + str([id for id in range(10,highest_seat_id_a) if id not in list_of_seat_ids]))          
+    
+    
+
+
