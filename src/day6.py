d461affd9e67850e18173724c8b12f6b89b9290d
Sujatha Nagarajan <sujatha.wgp@gmail.com>
2020-12-06 22:55:43 -0600

    day 6

---
+++ src/day6.py
@@ -0,0 +1,40 @@
+from util import *
+import string
+
+rows = get_file_contents("../input/day6.txt")
+
+ans_a = 0
+group_ascii = [l for l in string.ascii_lowercase]
+for i in range(len(rows)):
+    if rows[i] == '':
+        # reset 
+        group_ascii = [l for l in string.ascii_lowercase]
+    else:
+        for l in rows[i]:
+            if l in group_ascii:
+                group_ascii.remove(l)
+                ans_a += 1
+
+print("a) " + str(ans_a))
+
+ans_b = 0
+persons_in_a_group_count= 0
+ans_map = {}
+for i in range(len(rows)):
+    if rows[i] == '':
+        # you need to identify the questions to which everyone in a group answered "yes"
+        for l,count in ans_map.items():
+            if count == persons_in_a_group_count:
+                ans_b += 1
+
+        # reset
+        persons_in_a_group_count= 0
+        ans_map = {}
+    else:
+        persons_in_a_group_count += 1
+
+        # map how many persons answers "yes" to which questions
+        for l in rows[i]:
+            ans_map[l] = ans_map.get(l, 0) + 1
+
+print("b) " + str(ans_b))
--- src/util.py
+++ src/util.py
@@ -1,6 +1,9 @@
-def get_sanitized_file_contents(filename):
+def get_file_contents(filename):
     rows = []
     with open(filename,"r") as file:
         for line in file:
             rows.append(line.rstrip()) # remove trailing newline whitespaces etc
+    
+    # add empty last line so that all the entries will be processed properly
+    rows.append('')
     return rows
